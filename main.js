c34d87a8e08649384c82fca91c8be856166fd848
n-shuhei <91367184+n-shuhei@users.noreply.github.com>
2021-09-25 17:24:52 +0900

    Initial commit
    練習用

---
+++ main.js
@@ -0,0 +1,8 @@
+// h2要素を取得
+const h2 = document.querySelector{'h2'};
+
+h2.addEventListener('click', () => {
+    h2.textContent = 'Changed!';
+});
+
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
